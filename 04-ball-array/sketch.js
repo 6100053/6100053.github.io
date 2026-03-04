d8a5571656f83138bb41816eea25633785c040a1
6100053 <6100053@spsd.sk.ca>
2026-03-04 15:15:43 -0600

    Update ball demo

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -1,6 +1,8 @@
 // Object Array Demo
 
 let gravity = 0.5;
+let bounce = 0.5;
+let friction = 0.99;
 
 let ballArray = [];
 
@@ -11,38 +13,51 @@ function setup() {
 function draw() {
   background(0);
   noStroke();
-  fill(255);
 
   for (let ball of ballArray) {
     // Move and Bounce
     ball.dy += gravity;
 
-    if (ball.x - ball.radius < 0 || ball.x + ball.radius > width) {
-      ball.dx *= -1;
-    }
-    if (ball.y - ball.radius < 0 || ball.y + ball.radius > height) {
-      ball.dy *= -1;
+    if (ball.y + ball.radius > height) {
+      ball.y += ball.dy * (-1 + bounce);
+      ball.dy *= -bounce;
     }
 
     ball.x += ball.dx;
     ball.y += ball.dy;
 
+    ball.dx *= friction;
+
+    // Teleport if needed
+    if (ball.x < 0 - ball.radius) {
+      ball.x = width + ball.radius;
+    }
+    else if (ball.x > width + ball.radius) {
+      ball.x = 0 - ball.radius;
+    }
+
     // Display
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius*2);
   }
 }
 
 function mousePressed() {
-  spawnBall();
+  for (i = 0; i <= 10; i++) {
+    spawnBall(mouseX, mouseY);
+  }
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let theBall = {
-    x: random(100, width - 100),
-    y: random(100, height - 100),
+    x: _x,
+    y: _y,
     dx: random(-20, 20),
-    dy: random(-5, 5),
-    radius: random(10, 40),
+    dy: random(-20, 20),
+    radius: random(5, 25),
+    r: random(255),
+    g: random(255),
+    b: random(255),
   };
   ballArray.push(theBall);
 }
