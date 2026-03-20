4ba57896f8b664f51b5c5bd1bf9303f326f99065
6100053 <6100053@spsd.sk.ca>
2026-03-20 15:26:17 -0600

    Created 2d grid assignment

--- grid-game-assignment/sketch.js
+++ grid-game-assignment/sketch.js
@@ -1,15 +1,30 @@
-// Project Title
-// Your Name
-// Date
+// 2d Grid Game Assignment
+// Carsen Waters
+// April 13 2026
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Extras for Experts:
+// - PLACEHOLDER
 
+
+const CELL_SIZE = 50;
+const MAP_SIZE = 10;
+let grid = emptyGrid(MAP_SIZE);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(255);
+}
+
+function emptyGrid() {
+  let newGrid = [];
+  for (let y = 0; y < MAP_SIZE; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < MAP_SIZE; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
 }
