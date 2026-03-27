0cff618478fcda330bbedbc26fe973502f6c269b
6100053 <6100053@spsd.sk.ca>
2026-03-27 15:27:15 -0600

    working on 2d game

--- grid-game-assignment/sketch.js
+++ grid-game-assignment/sketch.js
@@ -7,6 +7,8 @@
 
 //COMMENTS
 
+//MUST FIX why the Tail is not disspeairng???
+
 const KEY_LEFT = 37;
 const KEY_UP = 38;
 const KEY_RIGHT = 39;
@@ -23,7 +25,7 @@ const MAP_SIZE = 11;
 const VIEW_SIZE = 11;
 
 const EMPTY_CELL = {type: "empty"};
-const STARTER_PLAYER = {x: 0, y: 0, direction: "N", length: 3, color: {r: 0, g: 0, b: 0}};
+const STARTER_PLAYER = {x: 0, y: 0, xSpeed: 0, ySpeed: -1, length: 3, color: {r: 0, g: 0, b: 0}};
 
 let grid = emptyGrid(MAP_SIZE);
 let player;
@@ -55,16 +57,20 @@ function draw() {
 
 function playerDirection() {
   if (keyIsDown(KEY_UP) || keyIsDown(KEY_W)) {
-    player.direction = "N";
+    player.xSpeed = 0;
+    player.ySpeed = -1;
   }
   else if (keyIsDown(KEY_DOWN) || keyIsDown(KEY_S)) {
-    player.direction = "S";
+    player.xSpeed = 0;
+    player.ySpeed = 1;
   }
   else if (keyIsDown(KEY_RIGHT) || keyIsDown(KEY_D)) {
-    player.direction = "E";
+    player.xSpeed = 1;
+    player.ySpeed = 0;
   }
   else if (keyIsDown(KEY_LEFT) || keyIsDown(KEY_A)) {
-    player.direction = "W";
+    player.xSpeed = -1;
+    player.ySpeed = 0;
   }
 }
 
@@ -72,8 +78,9 @@ function updateCells() {
   for (let row of grid) {
     for (let cell of row) {
       if (cell.type === "body") {
-        if (millis() > cell.time) {
-          cell = EMPTY_CELL;
+        cell.age += 1;
+        if (cell.age > cell.player.length) {
+          cell = EMPTY_CELL;//clone???
         }
       }
     }
@@ -81,22 +88,10 @@ function updateCells() {
 }
 
 function movePlayers() {
-  grid[player.y][player.x] = {type: "body", player: player, time: millis() + player.length * 1000};
+  grid[player.y][player.x] = {type: "body", player: player, age: 0};
 
-  if (player.direction === "N") {
-    player.y -= 1;
-  }
-  else if (player.direction === "S") {
-    player.y += 1;
-  }
-  else if (player.direction === "E") {
-    player.x += 1;
-  }
-  else if (player.direction === "W") {
-    player.x -= 1;
-  }
-  player.x = (player.x + MAP_SIZE) % MAP_SIZE;
-  player.y = (player.y + MAP_SIZE) % MAP_SIZE;
+  player.x = (player.x + player.xSpeed + MAP_SIZE) % MAP_SIZE;
+  player.y = (player.y + player.ySpeed + MAP_SIZE) % MAP_SIZE;
 
   //Collision/interaction placeholder
 
@@ -119,6 +114,10 @@ function drawGrid() {
         }
       }
       square(x * cellSize, y * cellSize, cellSize);
+      if (gridCell.type === "head") {
+        fill(255);
+        circle(x * cellSize + cellSize / 2, y * cellSize + cellSize / 2, cellSize / 2);//change
+      }
     }
   }
 }
@@ -136,10 +135,12 @@ function emptyGrid() {
 
 function addPlayer() {
   let newPlayer = structuredClone(STARTER_PLAYER);
-  newPlayer.x = floor(random(MAP_SIZE + 1));
-  newPlayer.y = floor(random(MAP_SIZE + 1));
+  newPlayer.x = floor(random(MAP_SIZE));
+  newPlayer.y = floor(random(MAP_SIZE));
   newPlayer.color.r = random(200);
   newPlayer.color.g = random(200);
   newPlayer.color.b = random(200);
   player = newPlayer;
+
+  grid[player.y][player.x] = {type: "head", player: player};
 }
